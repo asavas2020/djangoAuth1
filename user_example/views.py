911aa5d1f20d2371e10f5e4584298257e33fa8e6
Gokhan <gknsvsdata@gmail.com>
2022-09-01 18:09:49 +0300

    login_required

--- user_example/views.py
+++ user_example/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'user_example/index.html')
 
+
+@login_required # burası login olmadan bu sayfayı göremezsin demek
 def special(request):
     return render(request, "user_example/special.html")
 
